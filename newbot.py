509df3e42a5949047a040cc3a1b1c000a49dc0a0
SebasADE <sebastianalejandro.diazespino@gmail.com>
2024-12-12 12:26:22 -0500

    creacion del bot

---
+++ newbot.py
@@ -0,0 +1,30 @@
+import random
+import string
+import discord, audioop, os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+token = os.getenv('dt')
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+
+
+
+bot.run(token)
